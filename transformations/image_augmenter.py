0bbb6f36e4b9f1d36d58bfd69475589d9efb6bfa
Sam Laborde-Balen <labordebalensam@gmail.com>
2025-02-16 16:11:27 +0000

    Fix: add missing augmentation and resizer files content

---
+++ transformations/image_augmenter.py
@@ -0,0 +1,13 @@
+from torchvision import transforms
+
+def augment_image(image, size=(256, 256)):
+    # Compose a pipeline with several random transformations to augment the image.
+    transform_pipeline = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(degrees=15),
+        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+        transforms.RandomResizedCrop(size, scale=(0.8, 1.0)),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        transforms.ToTensor()
+    ])
+    return transform_pipeline(image)
