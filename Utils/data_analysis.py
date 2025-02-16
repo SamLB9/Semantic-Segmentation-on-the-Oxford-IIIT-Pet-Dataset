529ed0d861fce6b7f5f4afba0e42ea178426f873
jakedugi <jake@jakedugan.com>
2025-02-16 16:49:53 +0000

    smallest

--- Utils/data_analysis.py
+++ Utils/data_analysis.py
@@ -21,6 +21,22 @@ class ImageAnalyzer:
 
         return max_width, max_height
 
+    def get_smallest_image_dimensions(self):
+        min_width = 0
+        min_height = 0
+
+        for filename in os.listdir(self.directory):
+            if filename.endswith('.jpg'):
+                filepath = os.path.join(self.directory, filename)
+                with Image.open(filepath) as img:
+                    width, height = img.size
+                    if width < min_width:
+                        min_width = width
+                    if height < min_height:
+                        min_height = height
+
+        return min_width, min_height
+    
 if __name__ == "__main__":
     #Get absolute path relative to script location
     script_dir = os.path.dirname(os.path.abspath(__file__))  # Directory of script
